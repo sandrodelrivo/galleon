f4ab9f7e9b8b484b52ebc36a5447110669ddb226
Sandro <sandro.delrivo@gmail.com>
2018-01-23 13:50:39 -0800

    Edit rotation speed

--- game/overworld/player/player.gd
+++ game/overworld/player/player.gd
@@ -2,30 +2,30 @@ extends KinematicBody2D
 
 var angle = 0
 var speed = 0
-var depressedSpeed = 0 # Speed when ship is rotating
+var depressed_speed = 0 # Speed when ship is rotating
 var rotation_speed = 0.05
-const SPEED_MAX = 100
-const SPEED_MIN = 0
-var speedIncrement = 10
+var SPEED_MAX = 40
+var SPEED_MIN = 0
+var speed_increment = 10
+var is_crashed = false
 var velocity = Vector2()
-
 func _fixed_process(delta):
 
-	depressedSpeed = speed * 0.75
+	depressed_speed = speed * 0.75
 	var angle = get_rot()
 
 
-	rotation_speed = 0.03 * speed/SPEED_MAX
+	rotation_speed = 0.015
 
 	if (Input.is_action_pressed("ui_left")):
 		set_rot(get_rot() + rotation_speed)
-		velocity.x = depressedSpeed * cos(angle)
-		velocity.y = -depressedSpeed * sin(angle)
+		velocity.x = depressed_speed * cos(angle)
+		velocity.y = -depressed_speed * sin(angle)
 
 	elif (Input.is_action_pressed("ui_right")):
 		set_rot(get_rot() - rotation_speed)
-		velocity.x = depressedSpeed * cos(angle)
-		velocity.y = -depressedSpeed * sin(angle)
+		velocity.x = depressed_speed * cos(angle)
+		velocity.y = -depressed_speed * sin(angle)
 	else:
 		velocity.x = speed * cos(angle)
 		velocity.y = -speed * sin(angle)
@@ -35,30 +35,28 @@ func _fixed_process(delta):
 	motion = move(motion)
 
 	if (is_colliding()):
-			var n = get_collision_normal()
-			motion = n.slide(motion)
-			velocity = n.slide(velocity)
-			move(motion)
+		is_crashed = true
+	else:
+		is_crashed = false
+
+
 
 
 func accelerate():
 	#print("Accelerate!")
 	if (speed < SPEED_MAX):
-		speed += speedIncrement
+		speed += speed_increment
 
 func deccelerate():
 	#print("Deccelerate")
 	if (speed > SPEED_MIN):
-		speed -= speedIncrement
+		speed -= speed_increment
 
 func _input(event):
-	#print("Event!")
 	if (event.is_action_pressed("ui_up")):
 		accelerate()
-		print("Speed: ", speed)
 	elif (event.is_action_pressed("ui_down")):
 		deccelerate()
-		print("Speed: ", speed)
 
 func _ready():
 	print("Player Script Ready!")
